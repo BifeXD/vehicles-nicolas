d134652e19bca1a2605340274438dc684cd21dd6
kaue-m-favero <kauemello12@gmail.com>
2019-09-30 20:34:18 -0300

    atualizando index.js

--- src/index.js
+++ src/index.js
@@ -16,8 +16,10 @@ const vehicles = require("./api/vehicles")
 app.use("/customers", customers)
 app.use("/vehicles", vehicles)
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 });
 
 
